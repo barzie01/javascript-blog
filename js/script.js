26f330c21e6598da64e7c3c016219ec6d27d885c
Bartosz Ziętak <bartekzietak01@gmail.com>
2021-05-29 17:31:12 +0200

    Generate list of titles with JavaScript

--- js/script.js
+++ js/script.js
@@ -39,8 +39,55 @@ const selectedArticle = document.querySelector(attributeOfElement);
 selectedArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
 
-for(let link of links) {
-    link.addEventListener('click', titleClickHandler);
+function generateTitleLinks(){
+    console.log('function generateTitleLinks is running');
+    const titlesList = document.querySelector(optTitleListSelector);
+
+/* remove contents of titleList */
+    titlesList.innerHTML = '';
+
+/* find all the articles and save them to variable: articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    let html ='';
+
+/* for each article */
+    for (let article of articles){
+        
+        /* get the article id */
+        const articleId = article.getAttribute('id');
+
+        /* find the title element */
+        const titleElement = article.querySelector(optTitleSelector);
+
+        /* get the title from the title element */
+        const articleTitle = titleElement.innerHTML;
+
+        /* create HTML of the link */
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+        // first way:
+        /* insert link into titleList */ 
+        // titlesList.insertAdjacentHTML("beforebegin", linkHTML);
+
+        // second way below:
+        /* insert link into html variable */
+        html = html + linkHTML;
+
+        console.log(html);
+    }
+    titlesList.innerHTML = html;
+
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
+
+    for(let link of links) {
+        link.addEventListener('click', titleClickHandler);
+    }
 }
+
+generateTitleLinks();
